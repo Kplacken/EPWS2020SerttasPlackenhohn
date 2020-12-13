51a1f9a1702649dc45518e7b767c1a48b3ea51d6
Kplacken <Kimberly.plackenhohn@web.de>
2020-12-13 19:27:42 +0100

    bearbeitet

---
+++ Coding/EP_Programmierung_mitSASS/src/js/overlay.js
@@ -0,0 +1,4 @@
+
+const openOverlay = (id) => { document.getElementById(id).style.display = "block" };
+
+const closeOverlay = (id) => { document.getElementById(id).style.display = "none" };
--- EP_Programmierung/src/js/overlay.js
+++
@@ -1,4 +0,0 @@
-
-const openOverlay = (id) => { document.getElementById(id).style.display = "block" };
-
-const closeOverlay = (id) => { document.getElementById(id).style.display = "none" };
