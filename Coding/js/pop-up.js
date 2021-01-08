4c82f308c0204fc98a7bf66a6fc720c86465c75a
Kimberly Plackenhohn <Kimblery.plackenhohn@web.de>
2021-01-08 23:44:31 +0100

    Audit 3

--- Coding/js/pop-up.js
+++ Coding/js/pop-up.js
@@ -2,7 +2,7 @@
 
 /*  https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_win_settimeout1*/
 
-var myVar;
+/*var myVar;
 
 function myFunction() {
  // myVar = setInterval(alertFunc, 10800000);
@@ -15,5 +15,26 @@ function myFunction1() {
 
 function alertFunc() {
   alert("Mach eine Übung und vergiss nicht genug zu trinken!");
+}*/
+
+
+var myVar = setInterval(myFunction, 10000);
+
+function myFunction() {
+  var txt;
+  if (confirm("Nimm dir doch mal eine Pause! Mach eine Übung und vergiss nicht genug zu trinken!")) {
+    txt="Schön das du eine Pause machst!"
+  } else {
+    setTimeout(alertFunc,3000)
+    txt="Du wirst in 3 sekunden noch einmal an deine Pause erinnert!"
+  }
+  document.getElementById("demo").innerHTML = txt;
+}
+
+function alertFunc() {
+  alert("Ich möchte nicht noch einmal an deine Pause erinnern!");
 }
  
+function myFunction1() {
+  myVar2 = clearInterval(myVar)
+}
