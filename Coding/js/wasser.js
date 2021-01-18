0f7bdecef9d2f7ffb7d8dbe90e3304cd437ca498
Kimberly Plackenhohn <Kimblery.plackenhohn@web.de>
2021-01-18 17:51:26 +0100

    Bearbeitet

--- Coding/js/wasser.js
+++ Coding/js/wasser.js
@@ -18,4 +18,31 @@ $(document).ready(function() {
         $input.change();
         return false;
     });
+    $('.wasserglas_07').click(function () {
+        var $input = $(this).parent().find('input');
+        var truncated1 = Math.ceil((parseFloat($input.val()) + 0.7) * 100)/100;
+        const runden1 = truncated1.toFixed(1)
+        var count1 = runden1;
+        $input.val(count1);
+        $input.change();
+        return false;
+    });
+    $('.wasserglas_05').click(function () {
+        var $input = $(this).parent().find('input');
+        var truncated1 = Math.ceil((parseFloat($input.val()) + 0.5) * 100)/100;
+        const runden1 = truncated1.toFixed(1)
+        var count1 = runden1;
+        $input.val(count1);
+        $input.change();
+        return false;
+    });
+    $('.wasserglas_03').click(function () {
+        var $input = $(this).parent().find('input');
+        var truncated1 = Math.ceil((parseFloat($input.val()) + 0.3) * 100)/100;
+        const runden1 = truncated1.toFixed(1)
+        var count1 = runden1;
+        $input.val(count1);
+        $input.change();
+        return false;
+    });
 });
