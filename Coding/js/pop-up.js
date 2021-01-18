b545bd3cb556e4267132f2f6aa5e17e9ce9cc7b9
Kimberly Plackenhohn <Kimblery.plackenhohn@web.de>
2021-01-18 17:37:09 +0100

    Bearbeitet

--- Coding/js/pop-up.js
+++ Coding/js/pop-up.js
@@ -2,11 +2,11 @@
 
 /*  https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_win_settimeout1*/
 
-/*var myVar;
+var myVar;
 
 function myFunction() {
  // myVar = setInterval(alertFunc, 10800000);
-  myVar = setInterval(alertFunc, 5000);
+  myVar = setInterval(alertFunc, 3000);
 }
 
 function myFunction1() {
@@ -15,40 +15,5 @@ function myFunction1() {
 
 function alertFunc() {
   alert("Mach eine Übung und vergiss nicht genug zu trinken!");
-}*/
-
-
-
-
-
-/*function interval(){
-  var myVar = setInterval(myFunction, 10000);
 }
-*/
-
-function myFunction() {
-  var txt;
-  if (confirm("Nimm dir doch mal eine Pause! Mach eine Übung und vergiss nicht genug zu trinken!"))
-   { myVar
-    txt="Du hast die Erinnerung angenommen"
-  } else {
-    setTimeout(alertFunc,3000)
-    txt="Du wirst in 3 sekunden noch einmal an deine Pause erinnert!"
-  }
-  document.getElementById("demo").innerHTML = txt;
-}
-
-function alertFunc() {
-  alert("Ich möchte nicht noch einmal an deine Pause erinnern!");
-}
-
-
-var myVar = setInterval(myFunction, 10000);
-
-function myFunction1() {
-  myVar2 = clearInterval(myVar)
-  txt="Du hast die Erinnerung gerade ausgestellt."
-  document.getElementById("demo").innerHTML = txt;
-}
-
-
+ 
